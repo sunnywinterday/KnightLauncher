d1a38f9bd61fca01b1c37480a3f5059aa6ae338b
Younis Turk <younisaturk@gmail.com>
2024-12-06 21:57:19 -0600

    Changed comment lines to supported comment method
    Read and heard that :: is not native and can sometimes cause issues during parsing.

--- scripts/KL_Update.bat
+++ scripts/KL_Update.bat
@@ -1,12 +1,12 @@
 @echo off
-:: by Crowfunder
-:: my gh: https://github.com/Crowfunder
-:: KnightLauncher gh: https://github.com/lucasluqui/KnightLauncher
+REM by Crowfunder
+REM my gh: https://github.com/Crowfunder
+REM KnightLauncher gh: https://github.com/lucasluqui/KnightLauncher
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-:: Set Constant Vars
-:: I have no idea how to insert a var into cutting from string.
-:: For now "download_url" is purely informational.
+REM Set Constant Vars
+REM I have no idea how to insert a var into cutting from string.
+REM For now "download_url" is purely informational.
 SET regkeys[1]="HKEY_CURRENT_USER\SOFTWARE\Grey Havens\Spiral Knights"
 SET regkeys[2]="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve\Steam"
 SET api_url="https://api.github.com/repos/lucasluqui/KnightLauncher/releases/latest"
@@ -16,26 +16,26 @@ SET javaurl="https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245057_d
 SET javafname=jre_kl.exe
 SET separator=--------------------------------------------------------------------------------
 
-:: gamepathfailsafe gets triggered once the script finds a suitable SK path.
-:: Later on, the script checks if it was triggered. 
-:: If it was not - user is notified and prompted to manually enter the SK folder path.
+REM gamepathfailsafe gets triggered once the script finds a suitable SK path.
+REM Later on, the script checks if it was triggered. 
+REM If it was not - user is notified and prompted to manually enter the SK folder path.
 SET /A gamepathfailsafe=0
 
-:: Check if the script is used inside the SK folder. 
-:: If that's the case we skip whole game path detection.
+REM Check if the script is used inside the SK folder. 
+REM If that's the case we skip whole game path detection.
 IF EXIST rsrc IF EXIST scenes IF EXIST code\projectx-pcode.jar ( 
     SET gamepath="%cd%"
     SET /A gamepathfailsafe=1
     GOTO Install 
 )
 
-:: Check if SK could've been installed anywhere. 
-:: If not, prompt the user to manually enter the SK folder path and skip detection.
-:: Simply, per every "RegKey Not Found" error, the script increments the noflag counter.
-:: It gets handled in manner:
-:: noflag = 0   User has to choose whether to install for Steam or for standalone.
-:: noflag = 1   Check which installation is present.
-:: noflag = 2   Prompt user to manually enter the SK folder path and skip detection.
+REM Check if SK could've been installed anywhere. 
+REM If not, prompt the user to manually enter the SK folder path and skip detection.
+REM Simply, per every "RegKey Not Found" error, the script increments the noflag counter.
+REM It gets handled in manner:
+REM noflag = 0   User has to choose whether to install for Steam or for standalone.
+REM noflag = 1   Check which installation is present.
+REM noflag = 2   Prompt user to manually enter the SK folder path and skip detection.
 SET /A noflag=0
 setlocal enabledelayedexpansion
 FOR /L %%i in (1,1,2) do (
@@ -65,7 +65,7 @@ IF !noflag! EQU 2 (
     GOTO Install
 )
 
-:: Check if it's standalone, if not, skip to SteamPath.
+REM Check if it's standalone, if not, skip to SteamPath.
 REG QUERY %regkeys[1]% >nul 2>&1
 IF %errorlevel% EQU 1 (
     SETLOCAL disabledelayedexpansion
@@ -80,8 +80,8 @@ IF %errorlevel% EQU 1 (
 )
 
 
-:: Getting Steam Folder Path. We check if SK is installed in default Steam folder.
-:: Otherwise we parse libraryfolders.vdf file and check every other folder.
+REM Getting Steam Folder Path. We check if SK is installed in default Steam folder.
+REM Otherwise we parse libraryfolders.vdf file and check every other folder.
 :SteamPath
 FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY %regkeys[2]% /v InstallPath`) DO (
     IF "%%B" EQU "" ( SET appdir=%%A) ELSE (
@@ -97,8 +97,9 @@ IF EXIST "%appdir%\steamapps\common\Spiral Knights" (
 SET listfile="%appdir%\steamapps\libraryfolders.vdf"
 
 
-:: If user has more than 9 steam library folders the script will commit die.
-:: I have no idea how to handle that.
+REM If user has more than 9 steam library folders the script will commit die.
+REM I have no idea how to handle that.
+REM I will take a look :)
 SET /A i=0
 SETLOCAL enabledelayedexpansion
 FOR /F "tokens=* skip=4 usebackq" %%a in (%listfile%) do (
@@ -110,7 +111,7 @@ FOR /F "tokens=* skip=4 usebackq" %%a in (%listfile%) do (
       )
 )
 
-:: We check every steam dir
+REM We check every steam dir
 SET /A Filesx=!i!
 FOR /L %%i in (1,1,!Filesx!) do (
     IF EXIST "!pathlist[%%i]!\steamapps\common\Spiral Knights" (
